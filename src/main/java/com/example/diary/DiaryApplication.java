d51e6647270d5c1eec989cefb4fe5be38040cbc8
Aleksei <kozakaleksei@gmail.com>
2021-04-24 19:44:04 +0300

    moved default jdbc timezone to bootstrap class

--- src/main/java/com/example/diary/DiaryApplication.java
+++ src/main/java/com/example/diary/DiaryApplication.java
@@ -3,11 +3,19 @@ package com.example.diary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DiaryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiaryApplication.class, args);
+    }
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kiev"));
     }
 
 }
